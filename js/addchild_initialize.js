0489b80e04ce6bdad0bec6926c91578bdcc54d1f
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-15 11:41:10 +0900

    Update addchild_initialize.js

--- js/addchild_initialize.js
+++ js/addchild_initialize.js
@@ -1,37 +1,66 @@
-$(window).load(function(){
-	const liffId = "1657662321-nR14gmQy";
-	initializeLiff(liffId);
-});
-
-function initializeLiff(liffId){
+const id = "1657662321-aDGpzkNK";
+window.onload = function(e){
 	liff.init({
-		liffId:liffId
+		liffId: id
 	}).then(() =>{
 		initializeApp();
 	}).catch((err) => {
+		window.alert(err);
 		console.log('LIFF Initialization failed ', err);
 	});
+};
+
+function initializeApp() {
+    // ログインチェック
+    if (liff.isLoggedIn()) {
+        //ログイン済
+
+    } else {
+        // 未ログイン
+        let result = window.confirm("LINE Loginしますか？");
+        if(result) {
+            liff.login();
+        }
+    }
 }
 
 function sendText(text){
-	liff.sendMessages([{
-		'type': 'text',
-		'text': text
-	}]).then(function(){
-		liff.closeWindow();
-	}).catch(funciont(error){
-		window.alert('Failed to send message ' + error);
-	});
+	if(!liff.isInClient()){
+		window.alert('This button is unavailable as LIFF is currently being opened in an external browser.');
+	}else{
+		liff.sendMessages([
+			{
+			type: 'text',
+			text: text
+			}
+		]).then(function(){
+			liff.closeWindow();
+		}).catch(function(error){
+			window.alert('Failed to send message ' + error);
+		});
+	}
 }
 
 const params = (new URL(document.location)).searchParams;
 const key = params.get('key');
 
-$(function(){
+$(function(){	
 	$('form').submit(function(){
-		const newName = document.getElementById('new-name').value;
-		const newOffice = document.getElementById('new-office').value;
-		const message= '${newName}\n${newOffice}';
+		
+		const genre = document.getElementById("genre").value;
+		const freetxt= document.getElementById("textarea").value;
+		let message="None. This is not message.";
+		/*
+		if(genre==='day'){
+			message = '[放課後デイサービスへのご相談・ご意見]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
+		}else if(genre==='service'){
+			message = '[他のサービスに関するご相談]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
+		}else if(genre==='care'){
+			message = '[子育てについてのご相談]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
+		}else if(genre==='else'){
+			message = '[その他]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
+		}
+		*/
 		sendText(message);
 		return false;
 	});
