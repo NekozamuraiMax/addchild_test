3cbdad8903ed831326601bf0f4b4a66d2032f5f5
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-15 11:45:31 +0900

    Add files via upload

--- js/addchild_initialize.js
+++ js/addchild_initialize.js
@@ -1,66 +1,37 @@
-const id = "1657662321-aDGpzkNK";
-window.onload = function(e){
+﻿$(window).load(function(){
+	const liffId = "1657662321-nR14gmQy";
+	initializeLiff(liffId);
+});
+
+function initializeLiff(liffId){
 	liff.init({
-		liffId: id
+		liffId:liffId
 	}).then(() =>{
 		initializeApp();
 	}).catch((err) => {
-		window.alert(err);
 		console.log('LIFF Initialization failed ', err);
 	});
-};
-
-function initializeApp() {
-    // ログインチェック
-    if (liff.isLoggedIn()) {
-        //ログイン済
-
-    } else {
-        // 未ログイン
-        let result = window.confirm("LINE Loginしますか？");
-        if(result) {
-            liff.login();
-        }
-    }
 }
 
 function sendText(text){
-	if(!liff.isInClient()){
-		window.alert('This button is unavailable as LIFF is currently being opened in an external browser.');
-	}else{
-		liff.sendMessages([
-			{
-			type: 'text',
-			text: text
-			}
-		]).then(function(){
-			liff.closeWindow();
-		}).catch(function(error){
-			window.alert('Failed to send message ' + error);
-		});
-	}
+	liff.sendMessages([{
+		'type': 'text',
+		'text': text
+	}]).then(function(){
+		liff.closeWindow();
+	}).catch(funciont(error){
+		window.alert('Failed to send message ' + error);
+	});
 }
 
 const params = (new URL(document.location)).searchParams;
 const key = params.get('key');
 
-$(function(){	
+$(function(){
 	$('form').submit(function(){
-		
-		const genre = document.getElementById("genre").value;
-		const freetxt= document.getElementById("textarea").value;
-		let message="None. This is not message.";
-		/*
-		if(genre==='day'){
-			message = '[放課後デイサービスへのご相談・ご意見]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
-		}else if(genre==='service'){
-			message = '[他のサービスに関するご相談]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
-		}else if(genre==='care'){
-			message = '[子育てについてのご相談]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
-		}else if(genre==='else'){
-			message = '[その他]\n\n(本文)' + freetxt + '(本文終わり)\n\n上記の内容で送信しました。';
-		}
-		*/
+		const newName = document.getElementById('new-name').value;
+		const newOffice = document.getElementById('new-office').value;
+		const message= '${newName}\n${newOffice}';
 		sendText(message);
 		return false;
 	});
